3c777d0a9ec5b64a5cac6488d2c3a2b6f220216e
Hieuab1308 <119374687+Hieuab1308@users.noreply.github.com>
2025-05-15 11:26:22 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        NumberProcessor np = new NumberProcessor();
-        int[] numbers = {10, 20, 30, 40};
-        String result = np.processNumbers(numbers);
-        System.out.println(result);
-    }
-}
