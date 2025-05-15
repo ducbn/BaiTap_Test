a9fc25edfd0614126d8180bc2bd53cbb91e6b556
Hieuab1308 <22010104@st.phenikaa-uni.edu.vn>
2025-05-15 11:38:38 +0700

    giai quyet van de 2

--- src/test/NumberProcessorTest.java
+++ src/test/NumberProcessorTest.java
@@ -23,27 +23,27 @@ public class NumberProcessorTest {
         assertEquals("Sum is greater than 100, even numbers: 2", result);
     }
 
-//    @Test
-//    public void testProcessNumbers_EmptyArray() {
-//        NumberProcessor np = new NumberProcessor();
-//        int[] nums = {};
-//        String result = np.processNumbers(nums);
-//        assertEquals("Sum is 100 or less, even numbers: 0", result);
-//    }
-//
-//    @Test
-//    public void testProcessNumbers_AllOdd() {
-//        NumberProcessor np = new NumberProcessor();
-//        int[] nums = {1, 3, 5, 7}; // sum = 16, evenCount = 0
-//        String result = np.processNumbers(nums);
-//        assertEquals("Sum is 100 or less, even numbers: 0", result);
-//    }
-//
-//    @Test
-//    public void testProcessNumbers_AllEven_SumEquals100() {
-//        NumberProcessor np = new NumberProcessor();
-//        int[] nums = {40, 60}; // sum = 100, evenCount = 2
-//        String result = np.processNumbers(nums);
-//        assertEquals("Sum is 100 or less, even numbers: 2", result);
-//    }
+    @Test
+    public void testProcessNumbers_EmptyArray() {
+        NumberProcessor np = new NumberProcessor();
+        int[] nums = {};
+        String result = np.processNumbers(nums);
+        assertEquals("Sum is 100 or less, even numbers: 0", result);
+    }
+
+    @Test
+    public void testProcessNumbers_AllOdd() {
+        NumberProcessor np = new NumberProcessor();
+        int[] nums = {1, 3, 5, 7}; // sum = 16, evenCount = 0
+        String result = np.processNumbers(nums);
+        assertEquals("Sum is 100 or less, even numbers: 0", result);
+    }
+
+    @Test
+    public void testProcessNumbers_AllEven_SumEquals100() {
+        NumberProcessor np = new NumberProcessor();
+        int[] nums = {40, 60}; // sum = 100, evenCount = 2
+        String result = np.processNumbers(nums);
+        assertEquals("Sum is 100 or less, even numbers: 2", result);
+    }
 }
